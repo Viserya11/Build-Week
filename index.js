4d65dfd31a9d15c442ad9ac9598e54476c4b2613
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-19 08:14:32 +0100

    100% completed welcome feature

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const check = document.getElementById("check")
+let btn = document.getElementById("button")
 
-function checked() {
-  if (check.checked == true) {
-    alert("checked!")
+btn.disabled = true
+
+function isChecked() {
+  if (check.checked) {
+    btn.disabled = false
   } else {
-    alert("Unchecked!")
+    btn.disabled = true
   }
 }
+
+function isDisabled() {
+  if (btn.disabled) {
+    console.log("button is disabled")
+  } else {
+    console.log("button is active")
+  }
+}
+
+//btn.addEventListener("click", isDisabled())
