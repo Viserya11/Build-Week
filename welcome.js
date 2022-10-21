c569fff1291d950c3c0c8eee300ee0d785789669
AndyCotruta <acotruta18@gmail.com>
2022-10-21 02:05:22 +0200

    Styled&Linked
    All Pages were checked for consistent styling and linked together.

--- welcome.js
+++ welcome.js
@@ -1,16 +1,19 @@
 const check = document.getElementById("check");
 let btn = document.getElementById("inactivebtn");
+let anchorTag = document.getElementById("anchorTag");
 
 check.addEventListener("click", () => {
   console.log(`the button is ${btn}`);
   btn.style.backgroundColor = "#01ffff";
+  anchorTag.classList.remove("disabled");
+  btn.classList.remove("disabled");
   btn.classList.add("enabledbtn");
 });
 
-const isChecked = () => {
-  if (check.checked === false) {
-  }
-};
+// const isChecked = () => {
+//   if (check.checked === false) {
+//   }
+// };
 
 /*const addBoxShadow = () => {
   btn.classList.toggle("activebtn")
