8fbd13e1a28d7aa6adfb06786e5e10f554f42543
AndyCotruta <acotruta18@gmail.com>
2023-03-27 18:36:56 +0200

    things

---
+++ Circular Progressbar/script.js
@@ -0,0 +1,18 @@
+// let progressBar = document.querySelector(".circular-progress");
+// let valueContainer = document.querySelector(".value-container");
+
+// let progressValue = 60;
+// let progressEndValue = 0;
+// let speed = 1000;
+
+// let progress = setInterval(() => {
+//   progressValue--;
+//   valueContainer.textContent = `${progressValue}`;
+//   progressBar.style.background = `conic-gradient(
+//       #f4e5ef ${progressValue * 6}deg,
+//       #ebbaee ${progressValue * 6}deg
+//   )`;
+//   if (progressValue == progressEndValue) {
+//     clearInterval(progress);
+//   }
+// }, speed);
